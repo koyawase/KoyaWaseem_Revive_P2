b59d16a9b6c247f187b407e7f4d14d6ef44c1280
Waseem Koya <waseemkoya@gmail.com>
2018-07-29 17:50:28 +1200

    created basic top down scene

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
+
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        controller.Move(moveDirection);
+	}
+}
