59e34d2abb80b735d4135e8ca19dbc578c915e30
Waseem Koya <waseemkoya@gmail.com>
2018-08-05 22:20:43 +1200

    created moving platforms

---
+++ Assets/Scripts/PlayerAttach.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttach : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player.transform.parent = null;
+    }
+
+
+}
