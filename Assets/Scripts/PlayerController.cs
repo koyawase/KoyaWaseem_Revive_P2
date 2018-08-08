7666862cc306950fe127438709bf4e8eb15afa38
Waseem <waseemkoya@gmail.com>
2018-08-09 10:58:48 +1200

    refactored player controller class

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,29 +22,38 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y,Input.GetAxis("Vertical") * moveSpeed);
         float yStore = moveDirection.y;
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
 
         if (controller.isGrounded) {
-            moveDirection.y = 0f;
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpForce;
-            }
-            if (Input.GetButton("LeftShift") && controller.isGrounded)
-            {
-                moveSpeed = sprintSpeed;
-            }
-            else
-            {
-                moveSpeed = defaultSpeed;
-            }
+            Jump();
+            Sprint();
         }
 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
 	}
+
+    void Jump()
+    {
+        moveDirection.y = 0f;
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButton("LeftShift") && controller.isGrounded)
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = defaultSpeed;
+        }
+    }
 }
