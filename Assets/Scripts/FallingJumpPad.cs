722f4996564dd5c3b7d14ae3533bc202bb61bded
Waseem <waseemkoya@gmail.com>
2018-08-09 11:23:39 +1200

    #3 created falling jump pad after use. However still exists in the world and just keeps falling.

---
+++ Assets/Scripts/FallingJumpPad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingJumpPad : MonoBehaviour
+{
+
+    PlayerController player;
+    private Rigidbody rigidbody;
+    public float power;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.moveDirection.y = 0;
+            player.moveDirection = player.moveDirection + (transform.up * power);
+            rigidbody.useGravity = true;
+        }
+
+    }
+}
