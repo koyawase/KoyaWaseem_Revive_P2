1af2e662bdd6369105df9bd1e8360ae90f79de97
Waseem <waseemkoya@gmail.com>
2018-08-09 11:10:51 +1200

    #2 created basic stamina parameter. Still need to clean it up. Possible in different stamina script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,14 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
     public float gravityScale;
     public CharacterController controller;
-    private float sprintSpeed;
-    private float defaultSpeed;
-
     public Vector3 moveDirection;
 
-	// Use this for initialization
-	void Start () {
+    private float sprintSpeed;
+    private float defaultSpeed;
+    public float stamina = 100f;
+
+    // Use this for initialization
+    void Start () {
         controller = GetComponent<CharacterController>();
         sprintSpeed = moveSpeed * 1.5f;
         defaultSpeed = moveSpeed;
@@ -47,13 +48,22 @@ public class PlayerController : MonoBehaviour {
 
     void Sprint()
     {
-        if (Input.GetButton("LeftShift") && controller.isGrounded)
+        if (Input.GetButton("LeftShift") && controller.isGrounded && stamina > 0)
         {
+            stamina = stamina - Time.deltaTime * 30f;
+            if (stamina < 0)
+            {
+                stamina = 0;
+            }
             moveSpeed = sprintSpeed;
         }
         else
         {
             moveSpeed = defaultSpeed;
+            if (stamina < 100 && !Input.GetButton("LeftShift"))
+            {
+                stamina = stamina + Time.deltaTime * 15f;
+            }
         }
     }
 }
