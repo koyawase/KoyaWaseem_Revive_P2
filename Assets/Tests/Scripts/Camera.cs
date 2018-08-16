760061635e93297facd3236f53beb48d05b5f521
Waseem <waseemkoya@gmail.com>
2018-08-16 12:12:55 +1200

    added vertical camera movement

--- Assets/Tests/Scripts/Camera.cs
+++ Assets/Tests/Scripts/Camera.cs
@@ -12,6 +12,12 @@ public class Camera : MonoBehaviour {
 
     void LateUpdate () {
         heading += Input.GetAxis("Mouse X") * Time.deltaTime * 200;
+
+        //Vertical camera.
+        tilt -= Input.GetAxis("Mouse Y") * Time.deltaTime * 200;
+        //Max at 80 degrees
+        tilt = Mathf.Clamp(tilt, -50f, 50f);
+
         transform.rotation = Quaternion.Euler(tilt, heading, 0);
 
         transform.position = player.position - transform.forward * cameraDistance + Vector3.up*playerHeight;
