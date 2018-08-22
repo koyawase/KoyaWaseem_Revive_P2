0c605e32927a8ccf6b56ea011a3127c2896dffd1
Waseem Koya <waseemkoya@gmail.com>
2018-08-22 13:25:29 +1200

    added final moving platform to reach the end #12

---
+++ Assets/Scripts/RaiseFinalPlatform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseFinalPlatform : MonoBehaviour {
+
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play("raiseFinalPlatform");
+    }
+}
